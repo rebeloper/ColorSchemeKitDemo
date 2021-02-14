595a9e2e1bb82816096534f6dbd74a94453dd74e
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-14 16:17:59 +0200

    Initial Commit

---
+++ Shared/ColorSchemeKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSchemeKitDemoApp.swift
+//  Shared
+//
+//  Created by Alex Nagy on 14.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorSchemeKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
